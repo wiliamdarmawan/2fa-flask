b368890a6e19f7490493cfea803d0df6e1ccbb8c
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-03-14 01:38:09 +0700

    add new flask-limiter requirement

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ import redis
 from app.config import Config
 from app.errors import register_error_handlers
 from celery import Celery
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 # Initialize extensions
 db = SQLAlchemy()
@@ -13,6 +15,10 @@ bcrypt = Bcrypt()
 jwt = JWTManager()
 redis_client = redis.StrictRedis(host="redis", port=6379, decode_responses=True)
 celery = Celery(__name__, include=["app.services.email_service"])
+limiter = Limiter(
+    key_func=get_remote_address,  # Use client's IP for rate limiting
+    storage_uri="redis://redis:6379"  # Use Redis as backend
+)
 
 def create_app():
     app = Flask(__name__)
@@ -21,6 +27,7 @@ def create_app():
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+    limiter.init_app(app)
     register_error_handlers(app)
 
     from app.routes.auth_routes import auth_bp
