ad5e35de479fc66290bfc8741c18bb4a5f368ce7
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-03-13 23:49:42 +0700

    add register without email test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -21,3 +21,42 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+def test_register_without_email(client):
+    response = client.post("/register", json={
+        "data": {
+            "attributes": {
+                "email": "",
+                "password" : "securepassword"
+            }
+        }
+    })
+    assert response.status_code == 400
+    assert response.json == {
+        "errors": [
+            {
+                "error": "Email and password required",
+                "errorCode": "TFAE2",
+                "errorHandling": "Please include the missing parameter."
+            }
+        ]
+    }
+
+    response = client.post("/register", json={
+        "data": {
+            "attributes": {
+                "password" : "securepassword"
+            }
+        }
+    })
+    assert response.status_code == 400
+    assert response.json == {
+        "errors": [
+            {
+                "error": "Email and password required",
+                "errorCode": "TFAE2",
+                "errorHandling": "Please include the missing parameter."
+            }
+        ]
+    }
+
