ef5996a2975064616db7cc196fa38c11a1be211f
Wiliam Darmawan <wiliam.darmawan@fazzfinancial.com>
2025-03-13 23:47:27 +0700

    create route for POST /register

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -10,3 +10,38 @@ import re
 import random
 
 auth_bp = Blueprint("auth", __name__)
+
+@auth_bp.route("/register", methods=["POST"])
+def register():
+    params = validate_json_api_params(request.json)
+
+    email = params.get("email")
+    password = params.get("password")
+
+    if not email or not password:
+        raise errors.MissingParamsError("Email and password required")
+    
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        raise errors.InvalidParamsError("Provided email is not an email address")
+
+    hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+    username = generate_username(email)
+
+    try:
+        user = User(email=email, username=username, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()  # Rollback to avoid partial commits
+        raise e
+
+    return jsonify(
+        {
+            "data": {
+                "id": user.id,
+                "attributes": {
+                    "username": user.username
+                }
+            }
+        }
+    ), 201
